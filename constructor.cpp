6a12797be30a2ecad045f884db0aecd41ad84c7b
Anushka Dubey <anushkadubey388@gmail.com>
2023-08-14 09:18:19 +0530

    commit

---
+++ constructor.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Hero
+{
+private:
+    int health;
+
+public:
+    char level;
+
+Hero(){
+    cout<<"Constructor called "<<endl;
+}
+    void print()
+    {
+        cout << level << endl;
+    }
+    // getter and setters -  if var health is private but we want to access it so we can use getter method
+
+    int getHealth()
+    {
+        return health;
+    }
+    char getLevel()
+    {
+        return level;
+    }
+    void setHealth(int h)
+    {
+        health = h;
+    }
+    void setLevel(char ch){
+        level = ch;
+    }
+};
+int main()
+{
+    //object created statically
+    cout<<"Hi "<<endl;
+    Hero Ironman;
+    cout<<"hello"<<endl;
+   
+
+//obj created dynamically
+Hero *h = new Hero();
+}
